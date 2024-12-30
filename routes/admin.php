05889476fa80bba3ae149bc09b4079b58ae0d1b2
Aamir Bashir <ksaak33@gmail.com>
2024-12-30 17:27:20 +0530

    Add routes for HouseController and update admin routes structure

--- routes/admin.php
+++ routes/admin.php
@@ -2,6 +2,8 @@
 
 use App\Http\Controllers\CategoryController;
 use App\Http\Controllers\CollectionController;
+use App\Http\Controllers\GarbageCollectionController;
+use App\Http\Controllers\HouseController;
 use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\RoleController;
 use App\Http\Controllers\UserController;
@@ -11,19 +13,21 @@ use App\Http\Controllers\SubCateoryController;
 use Illuminate\Support\Facades\Route;
 
 Route::prefix('admin')->name('admin.')->middleware(['auth', 'verified'])->group(function () {
-    Route::get('/dashboard',[ProfileController::class,'dashboard'])->name('dashboard');
+    Route::get('/dashboard', [ProfileController::class, 'dashboard'])->name('dashboard');
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-    Route::middleware(['role:admin'])->group(function(){
-        Route::resource('user',UserController::class);
-        Route::resource('role',RoleController::class);
-        Route::resource('permission',PermissionController::class);
-        Route::resource('category',CategoryController::class);
-        Route::resource('subcategory',SubCateoryController::class);
-        Route::resource('collection',CollectionController::class);
-        Route::resource('product',ProductController::class);
-        Route::get('/get/subcategory',[ProductController::class,'getsubcategory'])->name('getsubcategory');
-        Route::get('/remove-external-img/{id}',[ProductController::class,'removeImage'])->name('remove.image');
+    Route::middleware(['role:admin'])->group(function () {
+        Route::resource('user', UserController::class);
+        Route::resource('role', RoleController::class);
+        Route::resource('permission', PermissionController::class);
+        Route::resource('category', CategoryController::class);
+        Route::resource('subcategory', SubCateoryController::class);
+        Route::resource('collection', CollectionController::class);
+        Route::resource('product', ProductController::class);
+        Route::resource('house', HouseController::class);
+        // Route::resource('collection', GarbageCollectionController::class);
+        Route::get('/get/subcategory', [ProductController::class, 'getsubcategory'])->name('getsubcategory');
+        Route::get('/remove-external-img/{id}', [ProductController::class, 'removeImage'])->name('remove.image');
     });
 });
